0d0dc30d8e466892147d7ae07602824f02b2d7ba
김소민 <smkim21st@gmail.com>
2018-06-26 14:39:14 +0900

    18.06.26 김소민 오후 2시 반

---
+++ 김소민lab01.sql
@@ -0,0 +1,30 @@
+--실습 1)
+SELECT e.EMPNO
+      ,e.ENAME
+      ,e.JOB
+      ,e.SAL
+  FROM emp e
+  ORDER BY e.SAL desc
+;
+
+--실습 2)
+SELECT e.EMPNO
+      ,e.ENAME
+      ,e.HIREDATE
+  FROM emp e
+  ORDER BY e.HIREDATE
+;
+
+--실습 3)
+SELECT e.EMPNO
+      ,e.ENAME
+      ,e.COMM
+  FROM emp e
+  ORDER BY e.COMM
+;  
+
+--실습 4)
+SELECT 
+  FROM emp e
+  ORDER BY 
+;  
